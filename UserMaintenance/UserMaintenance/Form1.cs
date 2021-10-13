e628cf77a70a3b7408ed7d74eadc0e72b4891e6c
SzPal98 <sztrepka98@gmail.com>
2021-10-13 21:16:38 +0200

    fajlbairas commit

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,7 @@ namespace UserMaintenance
             label1.Text = Resource1.FullName;
             
             button1.Text = Resource1.Add;
+            button2.Text = Resource1.File;
 
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
@@ -34,6 +36,35 @@ namespace UserMaintenance
                 
             };
             users.Add(u);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SaveFileDialog sf = new SaveFileDialog();
+                if (sf.ShowDialog() != DialogResult.OK)
+                {
+
+                    return;
+                }
+
+                using (StreamWriter sw = new StreamWriter(sf.FileName, false, Encoding.UTF8))
+                {
+                    foreach (var item in textBox1.Text)
+                    {
+                        sw.Write(item.ToString());
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+            
+
         }
     }
 }
